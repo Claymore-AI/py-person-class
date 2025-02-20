f2f5a96ec9ca7a9c7be2c1282577e7021ada8c38
Claymore-AI <dimalishchuk2003@gmail.com>
2025-02-20 19:23:49 +0200

    used list_comperhension and method get

--- app/main.py
+++ app/main.py
@@ -8,16 +8,13 @@ class Person:
 
 
 def create_person_list(people: list) -> list:
-    list_people = []
-    for person in people:
-        list_people.append(Person(person["name"], person["age"]))
+    list_people = [Person(person["name"], person["age"]) for person in people]
 
     for person in people:
         person_instance = Person.people[person["name"]]
 
-        if "wife" in person and person["wife"] is not None:
+        if person.get("wife"):
             person_instance.wife = Person.people[person["wife"]]
-
-        if "husband" in person and person["husband"] is not None:
+        if person.get("husband"):
             person_instance.husband = Person.people[person["husband"]]
     return list_people
